8778a450541e87c3c6da8cfd4a19a97346a6bc0c
Blard <peteromu76@gmail.com>
2023-11-21 04:22:01 +0100

    rate limiting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express  from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import blogRouter from "./src/routes/blogRoutes.js";
 import authRouter from "./src/routes/authRoutes.js";
 import userRouter from "./src/routes/userRoutes.js";
@@ -17,6 +18,16 @@ dotenv.config();
 let corsOptions = { 
   origin : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'], 
 } 
+
+// rate limit
+// The blogApiLimiter mw will limit your IP address if you make 100 requests in 15min intervals
+
+const blogsApiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20, 
+  message: 'Too many requests from this IP, please try again later.',
+});
+
 // middleware
 app.use(cors(corsOptions))
 app.use(morgan('dev'))
@@ -38,14 +49,14 @@ app.use(passport.session());
 app.use('/', userRouter); 
 app.use('/', authRouter); 
 app.use('/auth', authRouter); 
-app.use('/', blogRouter);
+app.use('/',blogsApiLimiter, blogRouter);
+
 // After app.use(passport.initialize()) and app.use(passport.session())
 app.get('/profile', (req, res) => {
   if (req.isAuthenticated()) {
     console.log(req.user);
     res.send(`Welcome back blogg page`);
   } else {
-    // Handle unauthenticated access
     res.redirect('/login'); 
   }
 });
